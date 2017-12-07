7a814234836977db5988e04ef66d80303c867404
Vitaly <heavyvetal88@mail.ru>
2017-12-07 12:22:20 +0600

    Init repo

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var preFixer = require('gulp-autoprefixer');
+
+var bootstrapSass = {
+    in: './node_modules/bootstrap-sass/'
+};
+var source = 'src/';
+var fontsDir = source + 'fonts/';
+
+// compile scss
+var path = {
+    build: {
+        css: 'dist/'
+    },
+    src: {
+        style: source + '**/*.scss'
+    },
+    watch: source + '**/*.scss',
+    sassOpts: {
+        outputStyle: 'compact',
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets'],
+    },
+    clean: './build'
+};
+
+gulp.task('sass', function () {
+    return gulp.src(path.src.style)
+        .pipe(sass(path.sassOpts))
+        .pipe(concat('style.css'))
+        .pipe(preFixer({
+            browsers: ['last 2 versions']
+        }))
+        //.pipe(cssMin())
+        .pipe(gulp.dest(path.build.css))
+});
+
+//js
+gulp.task('js', function() {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        source + '**/*.js'
+    ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function(){
+    gulp.watch('src/**', ['sass', 'js'])
+    //gulp.watch('src/**', ['js'])
+});
---
+++ src/js/sit.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vitaly on 07.12.17.
+ */
+//close navigation on outside click
+$(document).on('click',function(){
+    $('#navbar-collapse').collapse('hide');
+});
