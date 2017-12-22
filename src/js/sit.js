02afa5952bf5905494c8f53e06c8843b9a9a604f
Evgeny <lord_storm@Alpha>
2017-12-22 09:21:38 +0600

    123

--- src/js/sit.js
+++ src/js/sit.js
@@ -1,3 +1,35 @@
 /**
  * Created by vitaly on 07.12.17.
  */
+$("#contactForm").submit(function(event){
+	// cancels the form submission
+	event.preventDefault();
+	submitForm();
+	console.log("submit!")
+});
+
+function submitForm(){
+	console.log("submitForm")
+	// Initiate Variables With Form Content
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var message = $("#message").val();
+
+	$.ajax({
+		type: "POST",
+		url: "php/form-process.php",
+		data: "name=" + name + "&email=" + email + "&message=" + message,
+		success : function(text){
+			console.log(text);
+			if (text){
+				formSuccess();
+				console.log("success")
+			} else {
+				console.log("jgfjhuukyguk");
+			}
+		}
+	});
+}
+function formSuccess(){
+	$( "#msgSubmit" ).removeClass( "hidden" );
+}
