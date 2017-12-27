4ff188b16a409d5653da9d0d05948bdfb27b1e6d
vitaly <heavyvetal88@mail.ru>
2017-12-27 14:10:46 +0600

    Add smooth scroll script

--- src/js/sit.js
+++ src/js/sit.js
@@ -34,9 +34,20 @@ function formSuccess(){
 	$( "#msgSubmit" ).removeClass( "hidden" );
 }
 
+// Add smooth scrolling on all links inside the navbar
 $(document).ready(function() {
-			$("a.first").fancybox();
-			$("a.two").fancybox();
-			$("a.video").fancybox({"frameWidth": 520,"frameHeight": 400});
-			$("a.content").fancybox({"frameWidth": 600,"frameHeight": 300});
-		});
+    $('#navbar-main a[href*=\"#\"]:not([href=\"#\"])').click(function () {
+
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 800);
+                return false;
+            }
+        }// End if
+
+    });
+});
