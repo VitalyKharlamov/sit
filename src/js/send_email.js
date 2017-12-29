24b576a7de18ec1f2c5da59bcfd1dd9e09279832
vitaly <heavyvetal88@mail.ru>
2017-12-29 17:01:39 +0600

    Fixes

---
+++ src/js/send_email.js
@@ -0,0 +1,29 @@
+/**
+ * Created by vitaly on 9/22/16.
+ */
+function sendmail() {
+    var KEY_EMAIL = "email=";
+    var KEY_MESSAGE = "&message=";
+    var e_mail = document.getElementById("e-mail").value;
+    var message = document.getElementById("messages").value;
+    //var result = "http://127.0.0.1/cgi/mail.py?email=test@test.com&message=Message%20is%20are%20good!&send_to=heavyvetal88@mail.ru";
+    var result = "http://127.0.0.1/cgi/mail.py?";
+    result += KEY_EMAIL + e_mail + KEY_MESSAGE + message;
+    sendMessage(result);
+}
+
+function sendMessage(getUrl) {
+
+    $.ajax({
+        url: getUrl,
+        type: 'GET',
+        success: function () {
+            console.log('Success!');
+            alert ('thank_you');
+            //window.location.href = 'index.html';
+        },
+        error: function () {
+            console.log('Uh Oh');
+        }
+    });
+}
