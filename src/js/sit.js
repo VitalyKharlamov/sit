38a823ff4bf529f06ffd25e402ed0b5b5cf2c826
vitaly <heavyvetal88@mail.ru>
2018-01-03 18:23:29 +0600

    Fix slider

--- src/js/sit.js
+++ src/js/sit.js
@@ -103,3 +103,23 @@ $(document).ready(function(){
     })
 
 });
+
+//Carousel script
+
+$('.carousel[data-type="multi"] .item').each(function () {
+    var itemToClone = $(this);
+
+    for (var i = 1; i < 2; i++) {
+        itemToClone = itemToClone.next();
+
+        // wrap around if at end of item collection
+        if (!itemToClone.length) {
+            itemToClone = $(this).siblings(':first');
+        }
+
+        // grab item, clone, add marker class, add to collection
+        itemToClone.children(':first-child').clone()
+            .addClass("cloneditem-" + (i))
+            .appendTo($(this));
+    }
+});
