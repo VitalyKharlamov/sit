a1c440bb9b710b974de31595f4dba71a7fc87551
vitaly <heavyvetal88@mail.ru>
2018-01-05 16:28:36 +0600

    Test messages

--- src/js/send_email.js
+++ src/js/send_email.js
@@ -12,6 +12,21 @@ function sendmail() {
     sendMessage(result);
 }
 
+function eventEnquiry() {
+    var KEY_EMAIL = "email=";
+    var KEY_MESSAGE = "&message=";
+    var KEY_NOTES = "&notes=";
+    var KEY_IS_NOTES = "&isNotes=";
+    var e_mail = document.getElementById("mail").value;
+    var message = document.getElementById("school_name").value;
+    var notes = document.getElementById("additional_notes").value;
+    var isNotes = true;
+    //var result = "http://127.0.0.1/cgi/mail.py?email=test@test.com&message=Message%20is%20are%20good!&send_to=heavyvetal88@mail.ru";
+    var result = "http://dev.keysib.ru/cgi/mail.py?";
+    result += KEY_EMAIL + e_mail + KEY_MESSAGE + message + KEY_NOTES + notes + KEY_IS_NOTES + isNotes;
+    sendMessage(result);
+}
+
 function sendMessage(getUrl) {
 
     $.ajax({
