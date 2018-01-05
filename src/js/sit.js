52004e5daedb949c26cc84af75bf5eb22d69c8c9
vitaly <heavyvetal88@mail.ru>
2018-01-05 16:09:33 +0600

    Remove not used code

--- src/js/sit.js
+++ src/js/sit.js
@@ -1,38 +1,6 @@
 /**
  * Created by vitaly on 07.12.17.
  */
-// $("#contact_form").submit(function(event){
-// 	// cancels the form submission
-// 	event.preventDefault();
-// 	submitForm();
-// 	console.log("submit!")
-// });
-
-function submitForm(){
-	console.log("submitForm");
-	// Initiate Variables With Form Content
-	var name = "NAME";
-	var email = $("#e-mail").val();
-	var message = $("#messages").val();
-
-	$.ajax({
-		type: "POST",
-		url: "php/form-process.php",
-		data: "name=" + name + "&email=" + email + "&message=" + message,
-		success : function(text){
-			console.log(text);
-			if (text){
-				formSuccess();
-				console.log("success")
-			} else {
-				console.log("jgfjhuukyguk");
-			}
-		}
-	});
-}
-function formSuccess(){
-	$( "#msgSubmit" ).removeClass( "hidden" );
-}
 
 // Add smooth scrolling on all links inside the navbar
 $(document).ready(function() {
