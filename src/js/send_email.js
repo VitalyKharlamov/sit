40aebe33bc8cb8293c2d95821a5b138e8902380b
Evgeny <lord_storm@Alpha>
2018-01-30 10:40:30 +0600

    page to send email

--- src/js/send_email.js
+++ src/js/send_email.js
@@ -1,14 +1,17 @@
 /**
  * Created by vitaly on 9/22/16.
  */
-function sendmail() {
+function sendmail($page) {
     var KEY_EMAIL = "email=";
     var KEY_MESSAGE = "&message=";
+    var KEY_PAGE = "&page=";
     var e_mail = document.getElementById("e-mail").value;
     var message = document.getElementById("messages").value;
+    var page = $page;
     //var result = "http://127.0.0.1/cgi/mail.py?email=test@test.com&message=Message%20is%20are%20good!&send_to=heavyvetal88@mail.ru";
-    var result = "http://squadintouch.com/cgi-bin/mail.py?";
-    result += KEY_EMAIL + e_mail + KEY_MESSAGE + message;
+    // var result = "http://squadintouch.com/cgi-bin/mail.py?";
+    var result = "http://dev.keysib.ru/cgi/mail.py?";
+    result += KEY_EMAIL + e_mail + KEY_MESSAGE + message + KEY_PAGE + page ;
     sendMessage(result);
 }
 
@@ -20,7 +23,8 @@ function eventEnquiry() {
     var message = document.getElementById("school_name").value;
     var notes = document.getElementById("notes").value;
     //var result = "http://127.0.0.1/cgi/mail.py?email=test@test.com&message=Message%20is%20are%20good!&send_to=heavyvetal88@mail.ru";
-    var result = "http://squadintouch.com/cgi-bin/mail.py?";
+    // var result = "http://squadintouch.com/cgi-bin/mail.py?";
+	var result = "http://dev.keysib.ru/cgi/mail.py?";
     result += KEY_EMAIL + e_mail + KEY_MESSAGE + message + KEY_NOTES + notes;
     sendMessage(result);
 }
