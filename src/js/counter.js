7e145f5d43ca6a71563690031bc6e9c8ed1921f4
vitaly <heavyvetal88@mail.ru>
2018-08-17 13:03:44 +0600

    Commented scroll event

--- src/js/counter.js
+++ src/js/counter.js
@@ -78,3 +78,13 @@ function animateValue(element, start, end, duration) {
         }
     }, stepTime);
 }
+
+//$(window).scroll(function() {
+//    var hT = $('#scroll-to').offset().top,
+//        hH = $('#scroll-to').outerHeight(),
+//        wH = $(window).height(),
+//        wS = $(this).scrollTop();
+//    if (wS > (hT+hH-wH)){
+//        console.log('H1 on the view!');
+//    }
+//});
